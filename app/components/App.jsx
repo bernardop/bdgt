180f5612ebbc9f5c49698ddc7abcfcaa2fd62826
Bernardo Pacheco <bernardo.pacheco@spectrumhealth.org>
2015-11-20 11:50:50 -0500

    Updated App and Note components to display model

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import uuid from 'node-uuid';
+import React, {Component} from 'react';
 import Note from './Note.jsx'
 
-export default () => {
-    return <Note />;
+const budgetItems = [
+    {
+        id: uuid.v4(),
+        name: 'Mortgage',
+        amount: 200.00
+    },
+    {
+        id: uuid.v4(),
+        name: 'Target',
+        amount: 34.50
+    },
+    {
+        id: uuid.v4(),
+        name: 'Meijer',
+        amount: 29.43
+    }
+];
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <ul>{budgetItems.map(this.renderBudgetItems)}</ul>
+            </div>
+        );
+    }
+
+    renderBudgetItems(budgetItem) {
+        return (
+            <li key={budgetItem.id}>
+                <Note itemName={budgetItem.name} amount={budgetItem.amount} />
+            </li>
+        );
+    }
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default class Note extends React.Component {
+export default class Note extends Component {
     render() {
-        return <div>Learn Webpack</div>;
+        return <div>{this.props.itemName} - ${this.props.amount}</div>;
     }
 }
