381c950c1c8381ba46c6f226ad4415f861685a07
Bernardo Pacheco <bernardo.pacheco@spectrumhealth.org>
2015-11-23 11:12:04 -0500

    Basic functionality for editing and deleting budget items

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,39 +1,88 @@
 import uuid from 'node-uuid';
 import React, {Component} from 'react';
-import Note from './Note.jsx'
-
-const budgetItems = [
-    {
-        id: uuid.v4(),
-        name: 'Mortgage',
-        amount: 200.00
-    },
-    {
-        id: uuid.v4(),
-        name: 'Target',
-        amount: 34.50
-    },
-    {
-        id: uuid.v4(),
-        name: 'Meijer',
-        amount: 29.43
-    }
-];
+import BudgetItems from './BudgetItems.jsx'
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            budgetItems: [
+                {
+                    id: uuid.v4(),
+                    name: 'Mortgage',
+                    amount: 200.00
+                },
+                {
+                    id: uuid.v4(),
+                    name: 'Target',
+                    amount: 34.50
+                },
+                {
+                    id: uuid.v4(),
+                    name: 'Meijer',
+                    amount: 29.43
+                }
+            ]
+        };
+    }
+
     render() {
+        const budgetItems = this.state.budgetItems;
+
         return (
             <div>
-                <ul>{budgetItems.map(this.renderBudgetItems)}</ul>
+                <button className="add-note" onClick={this.addBudgetItem}>+</button>
+                <BudgetItems items={budgetItems} onEdit={this.editBudgetItem}
+                    onDelete={this.deleteBudgetItem} />
             </div>
         );
     }
 
-    renderBudgetItems(budgetItem) {
-        return (
-            <li key={budgetItem.id}>
-                <Note itemName={budgetItem.name} amount={budgetItem.amount} />
-            </li>
-        );
+    addBudgetItem = () => {
+        this.setState({
+            budgetItems: [...this.state.budgetItems, {
+                id: uuid.v4(),
+                name: 'Costco',
+                amount: 231.98
+            }]
+        });
+    }
+
+    editBudgetItem = (itemId, amount) => {
+        const budgetItems = this.state.budgetItems;
+        const itemIndex = this.findBudgetItem(budgetItems, itemId);
+
+        if (itemIndex < 0) {
+            return;
+        }
+
+        budgetItems[itemIndex].amount = amount;
+
+        // shorthand - {budgetItems} is the same as {budgetItems: budgetItems}
+        this.setState({budgetItems});
+    }
+
+    findBudgetItem = (budgetItems, itemId) => {
+        const itemIndex = budgetItems.findIndex((item) => item.id === itemId);
+
+        if (itemIndex < 0) {
+            console.warn('Failed to find budget item', budgetItems, itemId);
+        }
+
+        return itemIndex;
+    }
+
+    deleteBudgetItem = (itemId) => {
+        const budgetItems = this.state.budgetItems;
+        const itemIndex = this.findBudgetItem(budgetItems, itemId);
+
+        if (itemIndex < 0) {
+            return;
+        }
+
+        this.setState({
+            budgetItems: budgetItems.slice(0, itemIndex).concat(budgetItems.slice(itemIndex + 1))
+        });
     }
 }
--- app/components/Note.jsx
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Note extends Component {
-    render() {
-        return <div>{this.props.itemName} - ${this.props.amount}</div>;
-    }
-}
