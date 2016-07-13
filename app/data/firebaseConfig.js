fbc72ce3cd8f89acef44486f9510638640c8768a
Bernardo Pacheco <berna.ipach@gmail.com>
2016-07-13 14:37:14 -0400

    Removed firebase config

--- app/data/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_CONFIG = {
-  // Initialize Firebase
-  apiKey: "AIzaSyCp4QdkO142qxiForb4_OcgyEB56DXepgE",
-  authDomain: "project-6623794559619252918.firebaseapp.com",
-  databaseURL: "https://project-6623794559619252918.firebaseio.com",
-  storageBucket: "project-6623794559619252918.appspot.com",
-}
-
-export default FIREBASE_CONFIG
