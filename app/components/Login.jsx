1328c599a1c2c3ea79765a07d65d64832a969f15
Bernardo Pacheco <berna.ipach@gmail.com>
2016-07-15 00:51:15 -0400

    Started working on login screen

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Grid, Col, Row } from 'react-flexbox-grid'
+import Textfield from 'material-ui/Textfield'
+import { Card, CardHeader } from 'material-ui/Card'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const cardStyle = {
+  padding: 30
+}
 
 export default class Login extends Component {
   render () {
     return (
-      <div>
-        Login
-      </div>
+      <Grid className='login-container'>
+        <Row center='xs' middle='xs' className='login-row'>
+          <Col xs={10} md={6}>
+            <Card containerStyle={cardStyle}>
+              <Textfield floatingLabelText='Email' />
+              <br />
+              <Textfield floatingLabelText='Password' type='password' />
+              <br />
+              <br />
+              <RaisedButton label='Login' primary={true} />
+            </Card>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
